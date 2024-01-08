238556b6e40497907ef0420367e06b1be266eb1a
Richard Janebo <richardjanebo@gmail.com>
2024-01-08 07:57:23 -0300

    Js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=43ea21e031c7dab6c62afb65a7df1e4c*/
 const apiKey = '43ea21e031c7dab6c62afb65a7df1e4c'
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + apiKey)
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}&lang=pt_br` )
+    var data = await response.json()
 
+    console.log(data)
+
+  
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  +"°C"
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"
+   
 }
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
+
