b6e0cd2c8a7ed25ad9f3edcbf2b82efc58321e22
Richard Janebo <richardjanebo@gmail.com>
2024-01-10 06:44:42 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiKey = '43ea21e031c7dab6c62afb65a7df1e4c'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 const searchBox = document.querySelector(".search input")
 const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}&lang=pt_br` )
@@ -15,6 +16,28 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  +"°C"
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"
+
+
+    if (data.weather[0].main == "Clouds"){
+        weatherIcon.src = "imagens/clouds.png"
+
+    }else if (data.weather[0].main == "Clear"){
+        weatherIcon.src = "imagens/clear.png"
+    }else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "imagens/drizzle.png"
+    }else if(data.weather[0].main == "Humidity"){
+        weatherIcon.src = "imagens/humidity.png"
+    }else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "imagens/mist.png"
+    }else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "imagens/rain.png"
+    }else if (data.weather[0].main == "Search"){
+        weatherIcon.src = "imagens/search.png"
+    }else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "imagens/snow.png"
+    }else if(data.weather[0].main == "Wind"){
+        weatherIcon.src = "imagens/Wind.png"
+    }
    
 }
 searchBtn.addEventListener("click", ()=>{
